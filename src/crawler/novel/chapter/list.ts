4658f8dea1970251c7e6e15c06d81c100280ca12
nowl-it <thh241005@gmail.com>
2025-02-11 08:09:59 +0700

    Fix maxPage bugging

--- src/crawler/novel/chapter/list.ts
+++ src/crawler/novel/chapter/list.ts
@@ -1,7 +1,6 @@
 import instance from '@/lib/instance'
 import string2html from '@/lib/string2html'
 import dotenv from 'dotenv'
-import apiJson from '@/crawler/json/api.json'
 import axiosRetry from 'axios-retry'
 
 dotenv.config()
@@ -9,14 +8,14 @@ dotenv.config()
 export default async function NovelChapter(novel_id: string, page: number = 1) {
     axiosRetry(instance as any, { retries: 10 })
 
-    const { data: pageHTML } = await instance.get(novel_id)
+    const { data: pageHTML } = await instance.get(novel_id + '/trang-' + page)
 
     const HTML = await string2html(pageHTML)
 
-    const title = HTML.title
-
     const maxPage = parseInt(
-        HTML.querySelector('.pagination > li:nth-child(8) > a:nth-child(1)')
+        HTML.querySelector(
+            '.pagination > li:nth-last-child(2) > a:nth-child(1)',
+        )
             ?.getAttribute('href')
             ?.replace((process.env.WEB_CRAWLER_URL ?? '') + '/', '')
             .split('/')[1]
